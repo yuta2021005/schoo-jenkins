ab6ea090b7ec0d27dbbf1193ed593431ebba10c7
yuta2021005 <110511103+yuta2021005@users.noreply.github.com>
2023-01-25 16:45:32 +0900

    Update deploy.bat

--- deploy.bat
+++ deploy.bat
@@ -1,2 +1,2 @@
 cd "%~pd0"
-copy target\schoo-jenkins.war "C:\Program Files\Apache Software Foundation\Tomcat 8.5\webapps\schoo-jenkins.war"
+copy target\schoo-jenkins.war "C:\Program Files\Apache Software Foundation\Tomcat 9.0\webapps\schoo-jenkins.war"
